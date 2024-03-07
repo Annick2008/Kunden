01e42ae2ec8f7c0619a43da9e389527ed85581ed
Yannick Kreuter <y.kreuterle@sbg.at>
2024-03-07 09:19:28 +0100

    add Kunde

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Kunden;
+
+Kunde k1 = new Kunde();
+Console.WriteLine("Rabatt: {0}", k1.GetRabatt());
+
+Vip k2 = new Vip();
+Console.WriteLine("Rabatt: {0}", k2.GetRabatt());
+ 
