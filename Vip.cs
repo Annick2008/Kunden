faccfa1222185f0a674d0bdcaa1f2c14969e0de9
Yannick Kreuter <y.kreuterle@sbg.at>
2024-03-07 09:16:19 +0100

    add Vip

---
+++ Vip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kunden;
+
+public class Vip : Kunde
+{
+    public override double GetRabatt()
+    {
+        // Was bedeutet das?
+        // return base.GetRabatt() + 5;
+        return 5;
+    }
+}
